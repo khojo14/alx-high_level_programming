56523d9180ab98208fce283f44b0cf637ee604b7
khojo14 <n.o.lamptey12@gmail.com>
2024-04-01 20:11:09 -0700

    Review for errors

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
+    from sys import argv
 
     args = len(argv) - 1
 
     if args == 0:
         print('0 arguments.')
-    elif args == 1:
-        print('1 argument:')
-        print('{}: {}'.format(args, argv[1]))
     else:
-        print('{} arguments:'.format(args))
-        for i in range args:
-            print('{} : {}'.format(i+1, argv[i+1]))
+        if args == 1:
+            print('1 argument:')
+        else:
+            print('{} arguments:'.format(args))
+
+        for i in range(args):
+            print('{}: {}'.format(i+1, argv[i+1]))
