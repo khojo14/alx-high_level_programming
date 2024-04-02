1a046e789b85a7a6b80eac310a6fe088fcd672ae
khojo14 <n.o.lamptey12@gmail.com>
2024-04-01 22:40:51 -0700

    Task 5 executable

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,5 +3,5 @@ if __name__ == "__main__":
     import hidden_4
 
     for n in dir(hidden_4):
-        if s[:2] =! "__":
-            print('{}'.format(s))
+        if n[:2] != "__":
+            print('{}'.format(n))
