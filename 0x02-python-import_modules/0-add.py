93caf6b9472f812148fe337d97695b186e193ae8
khojo14 <n.o.lamptey12@gmail.com>
2024-04-01 17:50:12 -0700

    Execution on import fixed

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -4,4 +4,8 @@ from add_0 import add
 a = 1
 b = 2
 
-print('{1} + {2} = {add(1,2)}'.format(a,b,add(a,b)))
+def main():
+    print('{} + {} = {}'.format(a,b,add(a,b)))
+
+if __name__ == "__main__":
+    main()
