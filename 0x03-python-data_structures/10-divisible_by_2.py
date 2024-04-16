f8ff58ea3020322e156d5c0130179d7c2a4775a1
khojo14 <n.o.lamptey12@gmail.com>
2024-04-16 11:57:14 -0700

    Typo fix

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def divisible_by_2(my_list=[]):
-    if mylist:
+    if my_list:
         new_list = []
         for num in my_list:
             if num % 2 == 0:
--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -2,7 +2,7 @@
 def max_integer(my_list=[]):
     if not my_list:
         return None
-    max_num = mylist[0]
+    max_num = my_list[0]
     for num in my_list:
         if max_num < num:
             max_num = num
