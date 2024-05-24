b00965099c3b095a41f7432df8c93ef55c1c877e
khojo14 <n.o.lamptey12@gmail.com>
2024-05-24 13:11:25 -0700

    New logic

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary == {} or is None:
+    if a_dictionary is None or not a_dictionary:
         return None
-    else:
-        new_dict = {}
-        value = 0
-        name_key = ""
-        for k, v in a_dictionary.items():
-            if value < v:
-                new_dict.clear()
-                new_dict[k] = v
-                value = v
-                name_key = k
-        return name_key
+
+    value = 0
+    key = None
+
+    for k, v in a_dictionary.items():
+        if value < v:
+            value = v
+            key = k
+
+    return key
