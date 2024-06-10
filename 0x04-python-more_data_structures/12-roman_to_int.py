d84085865aeb3de2f6ac904b5eb00f67d44fe91d
khojo14 <n.o.lamptey12@gmail.com>
2024-06-10 12:31:22 -0700

    Roman numerals

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,23 @@
+#!/usr/python3
+def roman_to_int(roman_string):
+    if not (roman_string) or roman_string is None:
+        return 0
+
+    roman_numerals = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                      'C': 100, 'D': 500, 'M': 1000}
+
+    numerical_value = 0
+    prev_value = 0
+
+    for char in reversed(roman_string):
+        if char in roman_numerals:
+            value = roman_numerals[char]
+            if value > prev_value:
+                numerical_value += value
+            else:
+                numerical_value -= value
+            prev_value = value
+        else:
+            return 0
+
+    return numerical_value
