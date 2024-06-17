d46e35db58d88b23fa10d8a604deded5582b18ba
khojo14 <n.o.lamptey12@gmail.com>
2024-06-17 05:57:20 -0700

    added return value

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,9 +1,13 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    nb_print = 0
+
     while True:
         try:
             for i in range(x):
                 print(my_list[i], end="")
+                nb_print += 1
             break
-        except IndexError:
+        except (IndexError, TypeError):
             break
+    return nb_print
