47771575b171c6240160db1b3205f0318c844a10
khojo14 <n.o.lamptey12@gmail.com>
2024-06-18 10:23:01 -0700

    colon for for statement

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -2,11 +2,10 @@
 def safe_print_list_integers(my_list=[], x=0):
     nb_print = 0
 
-    for i in range (x)
+    for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end="")
             nb_print += 1
-            break
         except(TypeError, ValueError):
             continue
         i += 1
