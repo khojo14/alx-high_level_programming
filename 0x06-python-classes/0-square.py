cf101a869f440d1c2d7206e81e25c9da94644bcf
khojo14 <n.o.lamptey12@gmail.com>
2024-06-25 08:02:10 -0700

    First task

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -5,4 +5,10 @@ This is an empty class that defines a Square
 
 
 class Square:
-    pass
+    def __init__(self):
+        """ This instantiates an empty class called Square
+        Args:
+            self (obj): is not to be passed
+
+        """
+        pass
