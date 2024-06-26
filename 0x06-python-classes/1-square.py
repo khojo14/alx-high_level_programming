7f61a6ffe4b01374405041a6e10a9ee57ff97823
khojo14 <n.o.lamptey12@gmail.com>
2024-06-25 17:55:44 -0700

    Second task + understanding Google Style Python Docstrings better

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines a class named square"""
+
+
+class Square():
+    """A class named Square"""
+    def __init__(self, size):
+        """Instantiation with the size of the Square
+
+        Args:
+            param1: the size of the Square
+        """
+        self.__size = size
