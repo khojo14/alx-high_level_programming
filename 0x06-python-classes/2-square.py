73a8145930b0f6b6d938b80fc600680f701157a9
khojo14 <n.o.lamptey12@gmail.com>
2024-06-25 18:44:55 -0700

    Docstrings

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
-"""define a class named square"""
+"""Defines a class named square."""
 
 
 class Square():
-    """A class named Square"""
+    """A class named Square."""
+
+
     def __init__(self, size=0):
         """Instantiation with the size of the Square
 
         Args:
-            param1: the size of the Square
+            size (int): the size of the Square
 
         Raises:
-            TypeError: if param1 is not an int
-            ValueError: if param1 < 0
+            TypeError: if size is not an int
+            ValueError: if size is less than 0
         """
-        try:
-            self.__size = size
-            if not size:
-                raise TypeError("size must be an integer")
-            if size < 0:
-                raise ValueError("size must be >= 0")
+        if not isintance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
