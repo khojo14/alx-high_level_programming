17ef0286111387ef49149a4f035c3de7e6f015d1
khojo14 <n.o.lamptey12@gmail.com>
2024-07-01 08:06:07 -0700

    Loop variables

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -79,5 +79,5 @@ class Square():
         else:
             for i in range(self.__size):
                 [print(" ", end="") for j in range(0, self.__position[0])]
-                [print('#', end='') for j in range(0, self.__size)]
+                [print('#', end='') for k in range(0, self.__size)]
                 print()
