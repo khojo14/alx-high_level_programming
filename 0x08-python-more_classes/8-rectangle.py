0b33aa9cd1a750c2a11737e8d8dd4123c4ec6df1
khojo14 <n.o.lamptey12@gmail.com>
2024-08-13 12:08:38 -0700

    Corrected decorator to @staticmethod

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -106,7 +106,7 @@ class Rectangle:
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
 
-    @classmethod
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """Compares two rectangle areas and returns the bigger one"""
         if not isinstance(rect_1, Rectangle):
