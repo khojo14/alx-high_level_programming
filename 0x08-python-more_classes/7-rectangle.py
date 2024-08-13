eedec70ab766c3dd78fd568612a452e6201c7471
khojo14 <n.o.lamptey12@gmail.com>
2024-08-13 11:23:53 -0700

    Changes to match examples used

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -93,7 +93,7 @@ class Rectangle:
         """returns a string representation of the rectangle with '#'"""
         string = ""
         if self.__width != 0 and self.__height != 0:
-            string += "\n".join(Rectangle.print_symbol * self.__width
+            string += "\n".join(str(self.print_symbol) * self.__width
                                 for j in range(self.__height))
         return string
 
