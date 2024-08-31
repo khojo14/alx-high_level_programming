da14cfd55f67514a5893acc6b2bee95c78bdc7a9
khojo14 <n.o.lamptey12@gmail.com>
2024-08-31 15:28:04 -0700

    README + Task 0

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+def lookup(obj):
+    """ returns the list of available attributes and methods of an object"""
+    list_obj = super(obj)
+    return list_obj
---
+++ 0x0A-python-inheritance/README.md
@@ -0,0 +1,1 @@
+ALX SE tasks on Python - Inheritance
