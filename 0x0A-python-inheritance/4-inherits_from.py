0c09eda3f72684829963e1b8800025e989614bf3
khojo14 <n.o.lamptey12@gmail.com>
2024-09-01 10:11:04 -0700

    Test for module docs

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,8 +5,5 @@ Contains the inherits_from function
 
 
 def inherits_from(obj, a_class):
-    """
-     returns True if the object is an instance of a class that inherited
-     (directly or indirectly) from the specified class ; otherwise False
-     """
-     return issubclass(type(obj, a_class) and type(obj) is not a_class)
+    """returns true if obj is a subclass of a_class, otherwise false"""
+    return(issubclass(type(obj), a_class) and type(obj) is not a_class)
