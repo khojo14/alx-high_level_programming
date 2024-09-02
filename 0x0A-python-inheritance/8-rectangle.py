b081cf02dad53214316ea670fd8a90b509cd9353
khojo14 <n.o.lamptey12@gmail.com>
2024-09-02 10:59:55 -0700

    typo

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -7,7 +7,7 @@ BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Retangle(BaseGeometry):
-    """A Rectangle class - subclass of BaseGeometry"""
+    """A rectangle class, subclass of BaseGeometry"""
     def __init__(self, width, height):
         """instantiation of Rectangle class"""
         super().integer_validator("width", width)
