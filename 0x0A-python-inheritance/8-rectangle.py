4536561585d54e4395106946e827007ee2ebdf15
khojo14 <n.o.lamptey12@gmail.com>
2024-09-02 10:56:29 -0700

    import superclass

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -3,12 +3,14 @@
 Defines a class
 """
 
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
 
 class Retangle(BaseGeometry):
     """A Rectangle class - subclass of BaseGeometry"""
     def __init__(self, width, height):
         """instantiation of Rectangle class"""
-        super().integer_validator(width)
+        super().integer_validator("width", width)
         self.__width = width
-        super().integer_validator(height)
+        super().integer_validator("height", height)
         self.__height = height
