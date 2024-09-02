90e2cbd9f3b0348ecdaec93eb9277d6dfb74918f
khojo14 <n.o.lamptey12@gmail.com>
2024-09-02 10:51:11 -0700

    __doc__ for __init__

--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -7,6 +7,7 @@ Defines a class
 class Retangle(BaseGeometry):
     """A Rectangle class - subclass of BaseGeometry"""
     def __init__(self, width, height):
+        """instantiation of Rectangle class"""
         super().integer_validator(width)
         self.__width = width
         super().integer_validator(height)
