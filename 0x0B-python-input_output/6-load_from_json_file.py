875b40b8b54d9b752f3e3cf391f437c345263dc2
khojo14 <n.o.lamptey12@gmail.com>
2024-09-24 07:18:36 -0700

    Separated functions

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -9,4 +9,4 @@ def load_from_json_file(filename):
     """creates an Object from a “JSON file”"""
     with open(filename, 'r', encoding="utf-8") as f:
         string = f.read())
-    json.loads(string)
+        json.loads(string)
